10e1b62acd7cc53a9b1f0a12e62f7e00e9fb64f3
Gideão <gideaoms@gmail.com>
2024-02-08 15:27:35 -0300

    add new utilities

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,10 @@
-import { and } from "./and";
-import { not } from "./not";
-import { or } from "./or";
+import { and } from "./and.js";
+import { not } from "./not.js";
+import { or } from "./or.js";
+import { greater } from "./greater.js";
+import { lesser } from "./lesser.js";
+import { equals } from "./equals.js";
+import { isError } from "./is-error.js";
 
 console.log("and", and(true, true, false)); // false
 
@@ -9,14 +13,22 @@ console.log("or", or(true, true, false)); // true
 console.log("not", not(true), not(false));
 
 // example
-let age = 20
-let city = "New York"
-let isAuthenticated = true
+let age = 20;
+let city = "New York";
+let isAuthenticated = true;
 
 if (and(age === 20, city === "New York")) {
-  console.log("it is here 1")
+  console.log("it is here 1");
 }
 
 if (or(isAuthenticated, city === "New York")) {
-  console.log("it is here 2")
+  console.log("it is here 2");
 }
+
+console.log("greater", greater(10, 5)); // true
+
+console.log("lesser", lesser(10, 5)); // false
+
+console.log("equals", equals(10, 10)); // true
+
+console.log("isError", isError(new Error())); // true
---
+++ src/equals.ts
@@ -0,0 +1,6 @@
+export function equals(
+  value1: number | string | boolean,
+  value2: number | string | boolean
+) {
+  return value1 === value2;
+}
