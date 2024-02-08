fe1e45757517ad1b8394cadb9472dd39e320e6ba
Gideão <gideaoms@gmail.com>
2024-02-08 15:06:51 -0300

    add example

--- src/app.ts
+++ src/app.ts
@@ -7,3 +7,12 @@ console.log("and", and(true, true, false)); // false
 console.log("or", or(true, true, false)); // true
 
 console.log("not", not(true), not(false));
+
+// example
+let age = 20
+let city = "New York"
+let isAuthenticated = true
+
+if (or(and(age === 20, city === "New York"), isAuthenticated)) {
+  console.log("it is here")
+}
