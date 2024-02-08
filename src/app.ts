f2e51fc03ed15a6eec605ba544ef683781bace8b
Gideão <gideaoms@gmail.com>
2024-02-08 12:13:17 -0300

    add comment

--- src/app.ts
+++ src/app.ts
@@ -2,8 +2,8 @@ import { and } from "./and";
 import { not } from "./not";
 import { or } from "./or";
 
-console.log("and", and(true, true, false));
+console.log("and", and(true, true, false)); // false
 
-console.log("or", or(true, true, false));
+console.log("or", or(true, true, false)); // true
 
 console.log("not", not(true), not(false));
